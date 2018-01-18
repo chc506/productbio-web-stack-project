3883e092c163cf1f7d762dd8da2f37aa0cb833a3
lausstsuai <enhaocui9437@gmail.com>
2018-01-17 22:28:44 -0800

    simple search done(backend, except price)

--- search/views.py
+++ search/views.py
@@ -5,9 +5,28 @@
 # @Last Modified by:   ahuynh
 # @Last Modified time: 2015-06-08 10:02:31
 from django.shortcuts import render_to_response
+import csv
 
+# Read csv file and search for corresonding attributes
 def search_csv( request ):
-	return {'query':2,'result':4}
+	request = request.split(',')
+	attributes = ['POST DATE','VENDOR','DESCRIPTION','QTY','INVOICE AMT']
+	res = {'result':[]}
+	with open('sample_data/San Francisco FY14-15.csv') as csvfile:
+		reader = csv.DictReader(csvfile, delimiter=',')
+		for row in reader:
+			row.pop('')
+			valid = True
+			for k, v in zip(attributes, request):
+				if not v:
+					continue
+				if row[k] != v:
+					valid = False
+					break
+			if valid:
+				res['result'].append(row)
+	return res
+
 
 def run_search( request ):
     ''' Search through our purchases '''
@@ -19,5 +38,5 @@ def run_search( request ):
         return render_to_response( 'noquery.html' )
 
     # TODO: Properly handle search query.
-    m = search_csv(request)
+    m = search_csv(query)
     return render_to_response( 'query.html', m)
